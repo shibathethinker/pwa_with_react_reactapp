3b2988f364ac98f9a755ec57c2effe103d6cd808
shibathethinker <shibassmb@gmail.com>
2018-10-12 21:04:23 +1100

    Updates

---
+++ precache-manifest.545ce2fa20448fa93ab95833fcc98d77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa_with_react_reactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f7bc0568cb297cd2a21f",
+    "url": "/pwa_with_react_reactapp/static/js/runtime~main.f7bc0568.js"
+  },
+  {
+    "revision": "ed55efa0045f778d6872",
+    "url": "/pwa_with_react_reactapp/static/js/main.ed55efa0.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/pwa_with_react_reactapp/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "ed55efa0045f778d6872",
+    "url": "/pwa_with_react_reactapp/static/css/main.bd59e036.chunk.css"
+  },
+  {
+    "revision": "a5812fa59032105231d753cbba0792c5",
+    "url": "/pwa_with_react_reactapp/index.html"
+  }
+];
